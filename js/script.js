23e7bab2808c2099a2709e873c6a84c6f337596f
大坪 俊平 <ootsuboshunpei@ootsubo-no-iMac.local>
2019-08-10 01:33:14 +0900

    Update form

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
-// function submitReserve(){
-//   alert("予約を受付けますか？");
-// }
+// console.log(1, getToday());
+
+window.addEventListener("load", getTomorrow);
+
+function getTomorrow(){
+  var today = new Date();
+  today.setDate(today.getDate() + 1);
+  var yyyy = today.getFullYear();
+  var mm = ("0" + (today.getMonth() + 1)).slice(-2);
+  var dd = ("0" + today.getDate()).slice(-2);
+  return  document.getElementById("reserve_date").value = yyyy + "-" + mm + "-" + dd;
+}
 
 new Vue({
   el: "#app",
   data: {
     v_phone_no: null,
     v_client_name: null,
-    v_reserve_date: null,
+    v_reserve_date: getTomorrow(),
     v_reserve_time: null,
     v_quantity_adult: 1,
     v_quantity_child: 0,
